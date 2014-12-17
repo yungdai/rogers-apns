ea7fd953ced01e17955feba5f25c4d754c66003c
Yung Dai <yungchidai@gmail.com>
2014-12-17 09:13:53 -0500

    push

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       if user.administrator?
         can :manage, :all
       else
-        can :manage, Apn, :active => true, :user_id => user.id
+        can :manage, Apn, :user_id => user.id
       end
     #
     # The first argument to `can` is the action you are giving the user
